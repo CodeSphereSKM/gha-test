7c02dbfaaed29312be2753e3939f8a775ec5f0fe
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2025-07-27 14:32:53 +0530

    Create main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:precise"
+}
